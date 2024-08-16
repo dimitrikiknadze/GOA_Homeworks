8978721895e7b04470ffd4cd5f30b2876c38b798
dimitrikiknadze <kiknadze.dimitri123@gmail.com>
2024-08-16 19:33:53 +0400

    commit

---
+++ day-27/homework/index.py
@@ -0,0 +1,19 @@
+# name = "dimitri"
+# print(name)
+
+
+# name = input("please enter your name: ")
+# print(name)
+
+
+# for i in range(5, 1, -2):
+#     print(i)
+
+# i = 0
+# while i < 5:
+#     if 1 % 1 == 0:
+#         print(i)
+#         i += 1
+
+# list = ["dima", 4, "bmw", "mercedes", "nissan"]
+# print(list)
