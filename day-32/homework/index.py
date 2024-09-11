ec2ef6efe89884e04db4daded9e1bc6df59c6219
dimitrikiknadze <kiknadze.dimitri123@gmail.com>
2024-09-11 15:03:52 +0400

    commit

---
+++ day-32/homework/index.py
@@ -0,0 +1,18 @@
+# def calculate_area(length, width):
+#     print(length * width)
+
+# calculate_area(7, 4)
+
+
+
+
+list_1 = [1, 3, 5, 7, 9]
+list_2 = [2, 4, 6, 8, 10]
+
+def true_false(num):
+    if num == list_1:
+        print("Odd")
+    else:
+        print("even")
+
+true_false(input("please enter any number in range 0-11: "))
